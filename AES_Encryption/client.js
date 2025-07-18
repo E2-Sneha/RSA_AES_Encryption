535170b632b78a2fa608289783509fcce4f1b8dc
E2-Sneha <sneha.s@e2infosystems.com>
2025-07-18 14:38:14 +0530

    Add files via upload

--- AES_Encryption/client.js
+++ AES_Encryption/client.js
@@ -2,10 +2,7 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const key = Buffer.from(process.env.AES_SECRET_KEY, 'hex');
-console.log("Key Length:", key.length);
-console.log("Key:", key);
 const iv = crypto.randomBytes(12);
-console.log("IV Length:", iv);
 
 function encrypt(text) {
     const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
@@ -21,6 +18,6 @@ function encrypt(text) {
 }
 
 // Simulating sending data
-const message = "Secret password 123";
+const message = "Secret password 12345 tatoo 5667";
 const encryptedPayload = encrypt(message);
 console.log("Send this to server:", JSON.stringify(encryptedPayload,null, 2));
